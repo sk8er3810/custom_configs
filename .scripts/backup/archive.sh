a6e6beda83731d1c54aeb7633ca58023960950d1
Michael Deschu <michael.deschu@gmail.com>
2011-03-17 07:20:55 -0400

    archive backups

--- .scripts/backup/archive.sh
+++ .scripts/backup/archive.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-TARINPUT=/cygdrive/c/backup
-TAROUTPUT=/cygdrive/c/backup.tar
+TARINPUT=/cygdrive/t/backup
+TAROUTPUT=/cygdrive/t/backup.tar
 TAR=tar
 MD5=md5sum
-MD5OUTPUT=/cygdrive/c/backup.tar.$MD5
+MD5OUTPUT=/cygdrive/t/backup.tar.$MD5
 
 # Tar likes to be executed in the same directory as the archive 
 #cd /cygdrive/x
@@ -13,16 +13,9 @@ while [ ! -e $TARINPUT ] ; do
 done
 
 # Create an archive of the backups
-$TAR cf $TAROUTPUT $TARINPUT/hourly $TARINPUT/daily $TARINPUT/weekly $TARINPUT/monthly
+$TAR cf $TAROUTPUT $TARINPUT/weekly $TARINPUT/monthly
 
 $MD5 $TAROUTPUT > $MD5OUTPUT
-
-# Copy to external sata drive
-DEST=/cygdrive/t
-if [ ! -w $DEST ] ; then
-    read -p "Please connect to the server...";
-fi
-cp -f $TAROUTPUT $MD5OUTPUT $DEST
 
 # Copy to server
 DEST=/cygdrive/y/Mike
