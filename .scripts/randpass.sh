f6d54221fd26f737e9624eac5e4fd1a70c00d523
Michael Deschu <michael.deschu@gmail.com>
2012-03-15 14:03:55 -0400

    Tweaks to randpass script
    Adds header comment and checks for proper arguments

--- .scripts/randpass.sh
+++ .scripts/randpass.sh
@@ -1,6 +1,18 @@
 #!/bin/bash
+# This script takes char set and length
+# as args to create a random password
+# containing the characters in char_set
+# of a specified length
 
 #example
 # cat /dev/urandom | env LC_CTYPE=C tr -cd [:print:] | head -c 10; echo
 
-cat /dev/urandom | env LC_CTYPE=C tr -cd $1 | head -c $2; echo
+if [ -z $2 ]; then
+    echo "Usage: randpass.sh <char set> <length>";
+    exit 1;
+fi
+
+CHAR_SET=$1
+LENGTH=$2
+cat /dev/urandom | env LC_CTYPE=C tr -cd $CHAR_SET | head -c $LENGTH; echo
+
